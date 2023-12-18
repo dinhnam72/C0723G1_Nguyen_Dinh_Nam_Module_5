41595b77f2d28a346163cb3e005d9ee507d9bcd4
dinhnam72 <dinhnam20162011@gmail.com>
2023-12-18 15:58:29 +0700

    sua bài tập 2 ss01

--- ss01/bt/bt5.js
+++ ss01/bt/bt5.js
@@ -1,7 +1,7 @@
 // 5. Tìm phần tử đầu tiên trong mảng lớn hơn 3.
 const numbers = [2,3,5,7,10];
 const check  = numbers.find((n)=>n>3);
-if (check!==undefined){
+if (check){
     console.log("Mảng có phẩn tử đâu tiên lớn hơn 3: " +check)
 }else {
     console.log("Không có phần tử nào lớn hơn 3")
